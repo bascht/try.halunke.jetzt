322e60cd32d2f2b6140e7b4634aea30e6f0dc7ba
Sebastian Schulze <github.com@bascht.com>
2018-02-12 01:34:17 +0100

    Try to mount in production

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module TryHalunkeJetzt
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.rvt.whitelisted_ips = [ '127.0.0.1', '192.168.121.0/16', '172.17.0.0/16' ]
-    config.rvt.default_mount_path = '/repl'
+  #  config.rvt.default_mount_path = '/repl'
     #config.rvt.command = 'bundle exec halunke'
     config.rvt.command = "docker run --rm -ti --read-only --tmpfs /run --tmpfs /tmp halunke:latest"
     # Settings in config/environments/* take precedence over those specified here.
